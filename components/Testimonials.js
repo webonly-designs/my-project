b369f5e29387ada7945dc18dac7cd27107dbfcdd
Akshay Agarwal <agarwal.akshay003@gmail.com>
2022-06-24 00:00:30 -0500

    design changes

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -4,7 +4,7 @@ export default function Testimonials() {
   return (
     <div id="testimonials" className="max-w-6xl mx-auto my-10 p-5">
         <h2 className="mb-10 lg:mb-15 text-center text-xl lg:text-3xl font-bold text-gray-700">Testimonials 🚀</h2>
-        <div className="md:flex md:space-x-4 space-y-4"> 
+        <div className="md:flex md:space-x-4 space-y-4 items-center justify-center"> 
             <img src="/fiverrReview.jpg" alt="review" className="w-full md:w-1/2 object-contain shadow-md rounded-lg"/>
             <img src="/twitter.jpg" alt="review" className="w-full md:w-1/2 object-contain shadow-md rounded-lg"/>
         </div>
